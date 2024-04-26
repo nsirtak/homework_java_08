bd2c528c69fe90f9cb7f5ed9a0cd9689ab83fb35
nsirtak <nsirtak@gmail.com>
2024-04-26 18:25:36 -0500

    homework 1

---
+++ src/main/java/school/redrover/HW1_1.java
@@ -0,0 +1,19 @@
+package school.redrover;
+/*
+Задача №1
+Написать программу которая будет печатать/выводить следующий текст:
+3
+2
+1
+Поехали!!!
+ */
+public class HW1_1 {
+    public static void main(String[] args) {
+        System.out.println("3");
+        System.out.println("2");
+        System.out.println("1");
+        System.out.println("Поехали!!!");
+
+        //System.out.println("3\n2\n1\nПоехали!!!"); // another solution
+    }
+}
---
+++ src/main/java/school/redrover/HW1_3.java
@@ -0,0 +1,9 @@
+package school.redrover;
+/*
+Вывести/напечатать смайлик (не :), а настоящий смайлик одним символом).
+ */
+public class HW1_3 {
+    public static void main(String[] args) {
+        System.out.println("\uD83D\uDE00");
+    }
+}
